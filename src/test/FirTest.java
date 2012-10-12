4974403ff29b5433d507ac65d29e9f6bee94600f
Yu Fu <jerryfuyu@gmail.com>
2012-10-12 14:00:12 +0800

    excel test

---
+++ src/test/FirTest.java
@@ -0,0 +1,17 @@
+package test;
+
+
+public class FirTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+	
+}
